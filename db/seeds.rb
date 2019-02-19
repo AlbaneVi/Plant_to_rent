4db07938a3a7a403ccf3f50fde40b95301913285
Albane Villard <albane.villard@gmail.com>
2019-02-19 12:32:17 +0100

    seeds plants (7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,71 @@
-plant1 = Plant.new ("citronnier", "arbre fruitier", "oui", "")
+plant1 = Plant.new (name: "citronnier",
+                    family: "arbre fruitier",
+                    description: "bel arbre d'une vingtaine d'année"
+                    water: "un peu"
+                    sun: "beaucoup"
+                    ext_int: "extérieur"
+                    size_cm: 180
+                    price_per_day: 12
+                    user_id:
+                    flowers: true
+                    photo: URL)
+
+plant2 = Plant.new (name: "cactus",
+                    family: "plantes grasses",
+                    description: "du piquant !"
+                    water: "un peu"
+                    sun: "à la folie"
+                    ext_int: "intérieur"
+                    size_cm: 25
+                    price_per_day: 6
+                    user_id:
+                    flowers: false
+                    photo: URL)
+
+plant3 = Plant.new (name: "orchidée",
+                    family: "plantes fleuries",
+                    description: "une floraison maginifique !"
+                    water: "beaucoup"
+                    sun: "beaucoup"
+                    ext_int: "extérieur"
+                    size_cm: 180
+                    price_per_day: 8
+                    user_id:
+                    flowers: true
+                    photo: URL)
+
+plant4 = Plant.new (name: "aubépine",
+                    family: "arbuste",
+                    description: "sympathique et romantique"
+                    water: "un peu"
+                    sun: "un peu"
+                    ext_int: "extérieur"
+                    size_cm: 80
+                    price_per_day: 11
+                    user_id:
+                    flowers: true
+                    photo: URL)
+
+plant5 = Plant.new (name: "monstera",
+                    family: "plantes vertes",
+                    description: "une plante monstrueuse !"
+                    water: "un peu"
+                    sun: "un peu"
+                    ext_int: "intérieur"
+                    size_cm: 240
+                    price_per_day: 18
+                    user_id:
+                    flowers: false
+                    photo: URL)
+
+plant6 = Plant.new (name: "ficus benjamina",
+                    family: "plantes vertes",
+                    description: "plante prometteuse"
+                    water: "un peu"
+                    sun: "beaucoup"
+                    ext_int: "intérieur"
+                    size_cm: 180
+                    price_per_day: 3
+                    user_id:
+                    flowers: false
+                    photo: URL)
