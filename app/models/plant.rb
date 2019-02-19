d0a2b38e1cb5afce634f77387f84330d5b2c2b37
Albane Villard <albane.villard@gmail.com>
2019-02-19 15:52:08 +0100

    seeds ok

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,7 +3,7 @@ class Plant < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :family, :flowers, presence: true
+  validates :name, :family, presence: true
   validates :price_per_day, :size_cm, presence: true, numericality: true
   validates :description, :water, :sun, :ext_int, presence: true
 end
