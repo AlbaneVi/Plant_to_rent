924799dd53a5a62b001c43efbf71db0a310494b4
chloe ahkoon <ahkoon.c@gmail.com>
2019-02-19 11:02:44 +0100

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :plants  do
+    resources :booking , only: [:index, :show, :delete]
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
